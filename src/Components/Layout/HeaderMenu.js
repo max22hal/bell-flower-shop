a2df5e1fc967edc240484a4c78997cd6285a2269
Максим и Даша <maksimanddasha@MacBook-Air-Maksim.local>
2024-11-04 19:59:09 +0100

    fix menu styles

--- src/Components/Layout/HeaderMenu.js
+++ src/Components/Layout/HeaderMenu.js
@@ -13,11 +13,14 @@ const HeaderMenu = (props) => {
         <div className={stylesHeader.header__wrapper}>
 
             <nav className={stylesHeader.menu}>
-                <li>
-                    <li className={classes.join(' ')} >
-                        <button>Home</button>
+                { /*
+                    <li>
+                        <li className={classes.join(' ')} >
+                            <button>Home</button>
+                        </li>
                     </li>
-                </li>
+                    */
+                }
                 <li>
                     <li className={stylesHeader.menu__item} onClick={() => props.onScroll("weddingFlowers")}>
                         <button>Wedding flowers</button>
