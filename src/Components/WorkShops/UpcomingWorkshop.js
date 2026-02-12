ff1df930247cf54ac69b447c1bae13676139014f
maksim.khalikov <maksim.khalikov@bpmcompany.eu>
2026-02-12 14:33:04 +0100

    Updated workshops and header tags

--- src/Components/WorkShops/UpcomingWorkshop.js
+++ src/Components/WorkShops/UpcomingWorkshop.js
@@ -5,75 +5,65 @@ import WreathWithOranges from "../../images/Wreath_with_oranges.JPEG";
 import EmailForm from "../Forms/EmailForm";
 
 const UpcomingWorkshop = forwardRef((props, ref) => {
+  const wrapperStyles = [
+    stylesPage.section_more__wrapper,
+    stylesPage.wreath_details,
+  ];
 
-    const wrapperStyles = [stylesPage.section_more__wrapper, stylesPage.wreath_details];
-
-
-    return (
-        <div className={wrapperStyles.join(' ')} ref={ref}>
-            <div className={stylesPage.section_more}>
-                <p className={stylesPage.section_more__intro}>
-                    We invite you to a cozy Christmas wreath workshop where you can create your very own festive wreath and embrace the holiday spirit! In a warm, decorated setting with Christmas music and twinkling lights, you will be surrounded by a creative atmosphere and like-minded people!
-                </p>
-                <div className={stylesPage.section_more__content}>
-                    <div>
-                        <img className={stylesPage.section_more__smallpic} src={WreathWithOranges} alt="Red and gold christmas wreath"></img>
-                    </div>
-                    <div>
-                        <h3>
-                            What to Expect:
-                        </h3>
-                        <ul>
-                            <li>
-                                All necessary materials are provided, so you can focus entirely on crafting your wreath.
-                            </li>
-                            <li>
-                                Christmas music and decor that will set a festive, cozy mood.
-                            </li>
-                            <li>
-                                Hot Christmas drinks and delicious sweets.
-                            </li>
-                        </ul>
-                        <h3>
-                            Details:
-                        </h3>
-
-                        <ul>
-                            <li>
-                                <strong>Date & Time:</strong> December 7th, starting at 4:00 PM
-                            </li>
-                            <li className={stylesPage.masterclass_location}>
-                                <strong>Location:</strong> Utrecht,
-                                <a href="https://maps.app.goo.gl/2xWuX9xiHGbWhZBR9" target="_blank" rel="noopener noreferrer">
-                                    <div>
-                                        Cafe Ddadi
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <strong>Cost:</strong> €50 (excluding drinks and food)
-                            </li>
-
-                        </ul>
-                    </div>
-
-                </div>
-                <div className={stylesPage.section_more__content}>
-                    <div>
-
-                        <p>
-                            Register in advance to reserve your spot! This workshop is a perfect opportunity to create not just a wreath, but a little Christmas magic for your home!
-                        </p>
-                        <EmailForm header="Send me a message to register!" />
-                    </div>
-                    <div>
-                        <img className={stylesPage.section_more__pic} src={RnGWreathPic} alt="Christmas wreath with oranges"></img>
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div className={wrapperStyles.join(" ")} ref={ref}>
+      <div className={stylesPage.section_more}>
+        <p className={stylesPage.section_more__intro}>
+          Upcoming workshops will be announced soon! If you’d like to arrange a
+          private workshop for your company, family, or friends, feel free to
+          reach out to me. We’d love to create something special for you!
+        </p>
+        <div className={stylesPage.section_more__content}>
+          <div>
+            <img
+              className={stylesPage.section_more__smallpic}
+              src={WreathWithOranges}
+              alt="Red and gold christmas wreath"
+            ></img>
+          </div>
+          <div>
+            <h3>What to Expect:</h3>
+            <ul>
+              <li>
+                All necessary materials are provided, so you can focus entirely
+                on creating your piece.
+              </li>
+              <li>
+                A warm, cozy atmosphere with music and styling to inspire your
+                creativity.
+              </li>
+              <li>
+                Complimentary drinks and sweet treats to enjoy during the
+                workshop.
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+        <div className={stylesPage.section_more__content}>
+          <div>
+            <p>
+              Ready to plan a workshop for your team or a cozy gathering with
+              friends? Contact me to book your private session — we will create
+              something beautiful together!
+            </p>
+            <EmailForm header="Send me a message to apply for a workshop!" />
+          </div>
+          <div>
+            <img
+              className={stylesPage.section_more__pic}
+              src={RnGWreathPic}
+              alt="Christmas wreath with oranges"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 });
 
 export default UpcomingWorkshop;
